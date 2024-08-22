e5ddff0a4aa7ef75cf88a943076626151ab8338b
Martin Kihungi <mwangim4786@gmail.com>
2024-08-22 13:28:01 +0000

    0-uniq_users.sql 4

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,5 @@
 --- script that creates a table with unique users 
 -- script can be executed on any database
-DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
