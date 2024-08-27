3be8d71c69ea1c59231f0c28afb88cd93c5c491f
Martin Kihungi <mwangim4786@gmail.com>
2024-08-27 05:18:50 -0700

    9-insert_school.py

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" MongoDB in  Python using pymongo """
+
+def insert_school(mongo_collection, **kwargs):
+    """ inserts a new document in a collection based on kwargs: """
+    doc = mongo_collection.insert(kwargs)
+    return doc.inserted_id
